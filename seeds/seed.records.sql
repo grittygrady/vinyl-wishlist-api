bbe68cbaf5f062c05e14765b59b07e152a0a6cd6
Christopher Grady <grittygrady@gmail.com>
2020-11-30 09:54:10 -0800

    Created seeds

---
+++ seeds/seed.records.sql
@@ -0,0 +1,7 @@
+TRUNCATE records RESTART IDENTITY CASCADE;
+
+INSERT INTO records (id, title)
+  VALUES
+    (1, 'Van Halen - 1984'),
+    (2, 'The Beatles - Revolver'),
+    (3, 'Pearl Jam - Vitalogy');
