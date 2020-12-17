81b017082de3a5fa07d03bbe63103561fef539ef
Christopher Grady <grittygrady@gmail.com>
2020-12-17 08:37:41 -0800

    revert

--- src/app.js
+++ src/app.js
@@ -22,14 +22,6 @@ const morganOption = (NODE_ENV === 'production')
     knex
   });
 
-  app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
-
   app.set('trust proxy', 1);
 
   const inDev = config.NODE_ENV === 'development'
